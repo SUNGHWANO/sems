5328177af105dfae2823abad9ad4ebc04a5cc7e5
9g9g <metalkaka@naver.com>
2014-04-16 17:36:02 +0900

    .

--- sems/src/servlets/user90/UserDetailServlet.java
+++ sems/src/servlets/user90/UserDetailServlet.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import vo.SubjectVo;
 import vo.UserVo;
-import dao.SubjectDao;
 import dao.UserDao;
 
 @WebServlet("/subject/detail.bit")
