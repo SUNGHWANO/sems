7df28986621b531c40107851966bbde153a581d0
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-04-16 16:45:07 +0900

    .

--- sems/src/dao/CourseDao.java
+++ sems/src/dao/CourseDao.java
@@ -8,7 +8,7 @@ import vo.SubjectVo;
 public interface CourseDao{
 	void insert(CourseVo course) throws Throwable;
 	List<CourseVo> list(int pageNo, int pageSize) throws Throwable;
-	SubjectVo detail(int no) throws Throwable;
+	CourseVo detail(int no) throws Throwable;
 	void update(CourseVo course) throws Throwable;
 	void delete(int no) throws Throwable;
 }
