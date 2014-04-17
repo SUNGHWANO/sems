e9c3417c657be897e5235bbd3579f6711fa984fb
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-04-17 19:25:32 +0900

    k

--- sems/src/servlets/subject/SubjectDeleteServlet.java
+++ sems/src/servlets/subject/SubjectDeleteServlet.java
@@ -18,22 +18,7 @@ public class SubjectDeleteServlet extends HttpServlet {
 	protected void doGet(
 			HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-	
-		
-		// 1) 응답 헤더에 Refresh 명령어 심기 
-		//response.setHeader("Refresh", "1;url=list.bit?pageNo=1&pageSize=10");
-		
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>과목삭제</title>");
-		
-		// 2) 응답 내용에 Refresh 명령어 심기
-		//out.println("<meta http-equiv='Refresh'"
-		//		+ " content='1;url=list.bit?pageNo=1&pageSize=10'>");
-		out.println("</head><body>");
-		
 		try {
-			out.println("<h1>과목 삭제 결과</h1>");
 			
 			SubjectDao dao = (SubjectDao)this.getServletContext()
 					.getAttribute("subjectDao");
@@ -42,17 +27,12 @@ public class SubjectDeleteServlet extends HttpServlet {
 			
 			dao.delete(no);
 			
-			out.println("삭제 성공!");
-			
-			// 3) Redirect 처리
-			// - 콘텐츠를 출력하지 않기 때문에 => 이전에 출력한 내용은 취소된다.
+
 			response.sendRedirect("list.bit?pageNo=1&pageSize=10");
 			
 		} catch (Throwable e) {
-			out.println("오류 발생 했음!");
 			e.printStackTrace();
 		}
-		out.println("</body></html>");
 	}
 }
 
