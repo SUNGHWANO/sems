2d0e44e4cc0b1c2b489f5d2b6fbd8c1d616ed8da
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-04-21 16:58:56 +0900

    .

---
+++ sems/src/filters/authFilter.java
@@ -0,0 +1,71 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class authFilter implements Filter{
+
+	@Override
+  public void destroy() {}
+
+	
+	
+	@Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain next) throws IOException, ServletException {
+		// doFilter()가 호출될 때 넘어 오는 파라미터는
+		// HttpServletRequest와 HttpServletResponse 이다.
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse)response;
+		
+	  //1) 클라이언트가 요청한 URL 정보를 가져온다.
+		String servletPath = httpRequest.getServletPath();
+		
+		
+		//2) 만약 URL에 '/auth/' 문자열을 포함하고 있지 않다면
+		//	   로그인 여부 검사한다.
+		if(!servletPath.startsWith("/auth/")){
+			HttpSession session = httpRequest.getSession();
+			if(session.getAttribute("loginUser") == null){
+				String contextRoot = httpRequest.getContextPath(); // "/sems;"
+				httpResponse.sendRedirect(contextRoot + "/auth/login.bit");
+				// "/sems/auth/login.bit/"
+				return;
+			}
+		}
+		
+		next.doFilter(request, response);
+		
+		
+		//3) 그 밖에 모든 URL에 대해 로그인 여부 검사
+  }
+
+	@Override
+  public void init(FilterConfig arg0) throws ServletException {
+		  
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
