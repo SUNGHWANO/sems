9f097d8bb7d22d0ea0c23e039bee26c7e184d988
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-04-24 10:05:47 +0900

    123

---
+++ sems/src/controls/SubjectInsertControl.java
@@ -0,0 +1,38 @@
+package controls;
+
+import java.util.Map;
+
+import vo.SubjectVo;
+import dao.SubjectDao;
+
+public class SubjectInsertControl implements PageController{
+	SubjectDao subjectDao;
+	
+	public void setSubjectDao(SubjectDao subjectDao) {
+	  this.subjectDao = subjectDao;
+  }
+	
+	
+	@Override
+  public String execute(Map<String, Object> model) {
+	
+		
+		if(model.get("title") == null){
+			return "/subject/form.jsp";
+		}else{
+			
+			try{
+				
+			SubjectVo subject = new SubjectVo();
+			subject.setTitle((String)model.get("title"));
+			subject.setDescription((String)model.get("description"));
+			subjectDao.insert(subject);
+			model.put("subject", subject);
+			return "/subject/insert.jsp";
+			
+			}catch(Throwable ex){
+				throw new Error(ex);
+			}
+		}
+  }
+}
