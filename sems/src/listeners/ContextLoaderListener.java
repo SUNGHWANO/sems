cddedfb9646a9930188659fb2a438dcdc35af0aa
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-04-24 10:11:31 +0900

    .

--- sems/src/listeners/ContextLoaderListener.java
+++ sems/src/listeners/ContextLoaderListener.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletContextListener;
 
 import util.DBConnectionPool;
 import controls.SubjectDetailControl;
+import controls.SubjectInsertControl;
 import controls.SubjectListControl;
 import dao.MysqlSubjectDao;
 import dao.MysqlUserDao;
@@ -50,6 +51,9 @@ public class ContextLoaderListener implements ServletContextListener {
 		subjectDetailControl.setSubjectDao(subjectDao);
 		sc.setAttribute("/subject/detail.bit", subjectDetailControl);
 		
+		SubjectInsertControl subjectInsertControl = new SubjectInsertControl();
+		subjectInsertControl.setSubjectDao(subjectDao);
+		sc.setAttribute("/subject/insert.bit", subjectInsertControl);
 		
 	}
 
